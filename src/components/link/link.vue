bf854f26338485185461cdcd1c6b52346aa11c3b
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-01-23 12:07:52 +0100

    add routing and new link component; update dependencies

---
+++ src/components/link/link.vue
@@ -0,0 +1,24 @@
+// src/components/ColoredLink.vue
+<template>
+  <a 
+    :href="href" 
+    target="_blank" 
+    class="text-blue-500 underline flex items-end"
+    :class="className"
+  >
+    <slot></slot>
+    <ArrowUpRight 
+      :stroke-width="2.5" 
+      class="h-5 w-5" 
+    />
+  </a>
+</template>
+
+<script setup lang="ts">
+import { ArrowUpRight } from 'lucide-vue-next'
+
+defineProps<{
+  href: string
+  className?: string
+}>()
+</script>
---
+++ src/router/index.vue
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import About from "";
+import Projects from "";
+import Services from '';
+import Contact from "";
+
+
+// Define Routes
+const routes = [
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/projects",
+    name: "Project",
+    component: Projects,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
+  },
+];
+
+// Create Router Instance
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
