b0393086d35c1c42acb1bba48521d98f936d640e
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-01-24 15:08:09 +0100

    feat: update home page content and layout; enhance animations and add new links

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,54 +1,91 @@
+<!-- src/pages/index.vue -->
 <template>
-  <Navbar />
-  <!-- <div class="my-4">
-        <div class="content-section">
-          <h1 class="text-4xl lg:text-[50px]">
-            Engineering Seamless User Experiences.
-          </h1>
-          <div class="text-base lg:text-[20px] mt-4">
-            <p>
-              Software Engineer building scalable Backend Systems, Engaging User Interfaces,
-              Intelligent Bots & Web Scrapers. Creator of
-              <ColoredLink href="https://github.com/adedoyin-emmanuel/methane-cli">
-                Methane-Cli
-              </ColoredLink>
-            </p>
-  
-            <p class="mt-4">You can do great things from a small place.</p>
-  
-            <a
-              href="/Adedoyin-Emmanuel-Resume.pdf"
-              target="_blank"
-              class="inline-flex items-center gap-2 mt-4 px-4 py-2 border border-black rounded-full hover:bg-black hover:text-white transition-all"
+  <div>
+    <Navbar />
+
+    <div class="my-10">
+      <div class="content-section">
+        <h1 class="text-4xl lg:text-[50px]">
+          Transforming Digital Landscapes Engineering
+        </h1>
+        <br />
+        <br />
+        <div class="text-base lg:text-[20px] mt-4">
+          <p>
+            Software engineer specializing in intelligent automation,
+            user-friendly interfaces, and robust backend systems.
+          </p>
+
+          <div className="flex items-center gap-x-3 flex-wrap mt-4">
+            <ColoredLink
+              href="https://jamajama.com/"
             >
-              Resume <ArrowUpRight :stroke-width="1.5" />
-            </a>
+              JamaJama
+            </ColoredLink>
+            <ColoredLink
+              href="https://github.com/Adedoyin-Emmanuel/ercaspay-js-sdk"
+            >
+              Ercaspay-SDK
+            </ColoredLink>
+            <ColoredLink href="https://www.medistorepro.com/">
+                Medistorepro
+            </ColoredLink>
+            <ColoredLink href="https://link-magic.vercel.app/">
+              LinkMagic
+            </ColoredLink>
+            <ColoredLink href="https://github.com/iyanuloluwa-Miracle/TEAM-6-PURPLE">
+              Travel&Tourism API
+            </ColoredLink>
+            <ColoredLink href="https://gencode-sigma.vercel.app/">
+              GenCode
+            </ColoredLink>
           </div>
+
+          <br />
+          <p>
+            My engineering mindset focuses on scalable, efficient solutions that
+            push technical boundaries. I design solutions that address present
+            requirements while foreseeing potential difficulties.
+          </p>
+
+          <br />
+          <p class="italic text-gray-700">
+            "Great achievements emerge not from grand resources, but from
+            vision, dedication, and relentless innovation."
+          </p>
+
+          <br />
+          <br />
+
+          <a
+            href="/Adedoyin-Emmanuel-Resume.pdf"
+            target="_blank"
+            class="inline-flex items-center gap-2 mt-4 px-4 py-2 border border-black rounded-full hover:bg-black hover:text-white transition-all"
+          >
+            View Resume <ArrowUpRight :stroke-width="1.5" />
+          </a>
         </div>
-      </div> -->
+      </div>
+    </div>
+    <br>
 
-
-
-
-
-
-
-
-  <Footer/>
+    <Footer />
+  </div>
 </template>
+      
+  <script setup>
+import Navbar from "../../components/navbar/index.vue";
+import { ArrowUpRight } from "lucide-vue-next";
+import gsap from "gsap";
+import ColoredLink from "../../components/link/link.vue";
+import { onMounted } from "vue";
+import Footer from "../../components/footer/index.vue";
 
-
-
-
-<script>
-import Navbar from '../../components/navbar/index.vue';
-import Footer from '../../components/footer/index.vue';
-
-
-export default {
-    components: {
-        Navbar,
-        Footer
-    }
-}
+onMounted(() => {
+  gsap.fromTo(
+    ".content-section",
+    { opacity: 0, y: 100 },
+    { opacity: 1, y: 0, duration: 2, ease: "power3.out" }
+  );
+});
 </script>
