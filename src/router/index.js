2f6957f6d94a93b834a2e1a5d9d7b98831bca4d5
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-01-24 08:37:42 +0100

    add home, projects, and hall-of-fame routes; create home page component

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,31 @@
 import { createRouter, createWebHistory } from "vue-router";
 import About from "../views/about/page.vue";
+import Home from "../views/home/home.vue";
+import Projects from "../views/projects/page.vue";
+import HallOfFame from "../views/hall-of-fame/page.vue";
 
 // Define Routes
 const routes = [
-    {
+
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
     path: "/about",
     name: "About",
     component: About,
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+    component: Projects,
+  },
+  {
+    path: "/hall-of-fame",
+    name: "HallOfFame",
+    component: HallOfFame,
   },
   
 ];
